55eb557c4e4774338efe0a665fe6a3c250f6494d
Yavuz Selim <history.erciyes19@gmail.com>
2023-06-08 01:31:05 +0100

    US_06

--- src/test/java/myapp/tests/US_06_UserShopOnSite.java
+++ src/test/java/myapp/tests/US_06_UserShopOnSite.java
@@ -1,2 +1,17 @@
-package myapp.tests;public class US_06_UserShopOnSite {
+package myapp.tests;
+
+import org.testng.annotations.Test;
+
+public class US_06_UserShopOnSite {
+
+    @Test
+    public void userShouldAbleToShopOnTheSite(){
+        //The user should be able to find the desired product in the search box.
+        //The user should be able to add the product they are looking for to their cart. (ADD TO CART)
+        //User should see the items that have been added to the cart.
+        //The amount of products in the cart should be adjustable by the user.
+        //User should be able to see the Billing Address in order to purchase the products
+        //The user should be able to view and select payment options.
+        //The user should be able to complete their purchase.
+    }
 }
