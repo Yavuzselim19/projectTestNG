a4b162fb8b81632f504c437819c2e3b5d826eb71
Ozlem Sert <ozlemksert@gmail.com>
2023-06-08 20:17:27 +0200

    V2

--- src/test/java/myapp/tests/US_11_VendorLoginPageTest.java
+++ src/test/java/myapp/tests/US_11_VendorLoginPageTest.java
@@ -3,12 +3,13 @@ package myapp.tests;
 import myapp.pages.PearlyVendorHomePage;
 import myapp.pages.PearlyVendorLoginPage;
 import myapp.utilities.*;
-import org.bouncycastle.util.Arrays;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class US_11_VendorLoginPageTest {
@@ -36,15 +37,18 @@ public class US_11_VendorLoginPageTest {
         WaitUtils.waitFor(3);
         Assert.assertTrue(pearlyVendorHomePage.myAccountUpp.isDisplayed());
 
-/*
+
+        /*
+
         List<String> valuesList = Arrays.asList("Dashboard", "Store Manager", "Orders", "Downloads", "Addresses", "Account details", "Wishlist", "Support Tickets", "Followings", "Logout");
-        List<WebElement> elements = pearlyVendorHomePage.titles2;
+                List<WebElement> elements = pearlyVendorHomePage.titles2;
         for (int i = 0; i < elements.size(); i++) {
             System.out.println(elements.get(i).getText());
             ReusableMethods.verifyElementDisplayed(elements.get(i));
             Assert.assertTrue(elements.get(i).getText().equals(valuesList.get(i)));
         }
-        */
+
+         */
 
 
         ReusableMethods.verifyElementDisplayed(pearlyVendorHomePage.storeManager);
